129e1d55e79b94d2cde457d06b38a5e04c508e57
JCJordan <jordan.craig2@hotmail.co.uk>
2018-09-09 15:51:48 +0100

    Created Inventory class to hold players items.

---
+++ Source/Strength/Private/Inventory.cpp
@@ -0,0 +1,11 @@
+// Copyright FairgroundPanda Studios by Jordan J Craig
+
+#include "Inventory.h"
+
+FInventory::FInventory()
+{
+}
+
+FInventory::~FInventory()
+{
+}
---
+++ Source/Strength/Public/Inventory.h
@@ -0,0 +1,15 @@
+// Copyright FairgroundPanda Studios by Jordan J Craig
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class STRENGTH_API FInventory
+{
+public:
+	FInventory();
+	~FInventory();
+};
